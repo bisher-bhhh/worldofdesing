5ca24ebbe7d1a1db70e3f9e8c1f0b6c8e36c3684
eng.bichr.focusco <eng.bichr.focusco@gmail.com>
2024-08-23 08:56:11 +0300

    dd

--- resources/views/frontend/footer.blade.php
+++ resources/views/frontend/footer.blade.php
@@ -3,8 +3,8 @@
         <div class="container py-2">
             <div class="row py-4">
                 <div class="col d-flex align-items-center justify-content-center">
-                    <p><strong>world of designs</strong> - © Copyright 2024. All Rights Reserved </p>| Powered by
-                    <a href="https://bichrbraighle.worldofdesigns.ca/" style="color: #f6941c">  &nbsp;BH</a>
+                    <p><strong>world of designs</strong> - © Copyright 2024. All Rights Reserved | Powered by <a href="https://bichrbraighle.worldofdesigns.ca/" style="color: #f6941c">  &nbsp;BH</a> </p>
+
                 </div>
             </div>
         </div>
